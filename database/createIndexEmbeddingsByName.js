00cc9802338adf9f50e5d8011f5ed0ea36d82b5a
HardlinePizza21 <madridossa@gmail.com>
2025-05-21 13:12:07 -0500

    Pequeños cambios

--- database/createIndexEmbeddingsByName.js
+++ database/createIndexEmbeddingsByName.js
@@ -3,26 +3,26 @@ import dotenv from 'dotenv/config'
 
 const client = new MongoClient("mongodb+srv://HardlinePizza21:samuel14madrid@vectorsearch.oqgdznm.mongodb.net/?retryWrites=true&w=majority&appName=VectorSearch");
 
-async function run() {
+async function run(opt = 1) {
   try {
     const database = client.db("mi_base_de_datos");
     const collection = database.collection("vacantes");
    
     const index = {
-        name: "vector_index_by_nombre_vacante",
+        name: opt == 1 ? "vector_index_by_nombre_vacante": "vector_index" ,
         type: "vectorSearch",
         definition: {
           "fields": [
             {
               "type": "vector",
-              "path": "nombre_vacante_embedding",
+              "path": opt == 1?  "nombre_vacante_embedding": "embedding" ,
               "similarity": "dotProduct",
               "numDimensions": parseInt(process.env.DIMENSION_NUM)
             }
           ]
         }
     }
-    
+
     const result = await collection.createSearchIndex(index);
     console.log(result);
   } finally {
